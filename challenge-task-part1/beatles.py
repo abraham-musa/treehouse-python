48a46d69cfbc69c25b5d93941bba821f85127f29
Replit user <replituser@example.com>
2021-03-17 14:14:42 +0000

    Challenge Task 1 of 2

---
+++ challenge-task-part1/beatles.py
@@ -0,0 +1,8 @@
+# Challenge Task 1 of 2
+
+beatles = ["John"]
+others = ["George", "Ringo"]
+
+# Can you please add "Paul" to the end of the beatles list using the appropriate list method?
+
+beatles.append("Paul")
